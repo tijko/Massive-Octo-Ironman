37b3c8e3b6eaf664872d9ebc4c1b91c2b68274cb
tijko <oberon@oberon-Satellite-M645.(none)>
2013-01-14 23:41:40 -0500

    keep older versions

---
+++ versions/apod.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from urllib2 import urlopen
+from BeautifulSoup import BeautifulSoup
+
+class Apod(object):
+    def apod_wallpaper(self):
+        self.soup = BeautifulSoup(urlopen('http://apod.nasa.gov/').read())
+        self.pic = self.soup.find('img')
+        spot = str(self.pic).find('jpg')
+        back = spot + str(self.pic)[spot:].find('"')
+        with open('/home/oberon/Pictures/daily', 'wb') as pic_folder:
+            pic_folder.write(urlopen('http://apod.nasa.gov/' + str(self.pic)[10:back]).read())
+        pic_folder.close()
+        return 
+
+
+Apod().apod_wallpaper()
+ 
+## work in a way for 'png' and probably 'jpeg' ##
+
+
+
