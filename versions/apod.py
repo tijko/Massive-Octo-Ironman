d354945d0a5341fdc8bd68a776033ce68ccc511e
haumea <haumea@haumea-Satellite-M645.(none)>
2013-03-02 17:21:09 -0500

    edit

--- versions/apod.py
+++ versions/apod.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
+
+from os import environ
 from urllib2 import urlopen
 from BeautifulSoup import BeautifulSoup
 
+
 class Apod(object):
+
     def apod_wallpaper(self):
         self.soup = BeautifulSoup(urlopen('http://apod.nasa.gov/').read())
         self.pic = self.soup.find('img')
         spot = str(self.pic).find('jpg')
         back = spot + str(self.pic)[spot:].find('"')
-        with open('/home/oberon/Pictures/daily', 'wb') as pic_folder:
-            pic_folder.write(urlopen('http://apod.nasa.gov/' + str(self.pic)[10:back]).read())
-        pic_folder.close()
+        with open(os.environ['HOME'] + '/Pictures/daily', 'wb') as pic_folder:
+            pic_folder.write(urlopen('http://apod.nasa.gov/' + 
+                                      str(self.pic)[10:back]).read())
         return 
 
-
-Apod().apod_wallpaper()
+if __name__ == '__main__':
+    ap = Apod()
+    ap.apod_wallpaper()
  
-## work in a way for 'png' and probably 'jpeg' ##
-
-
-
