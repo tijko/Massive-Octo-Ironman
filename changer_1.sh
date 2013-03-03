50c6ce4b9f48e64bfea543ee835f6363e8a65f69
haumea <haumea@haumea-Satellite-M645.(none)>
2013-03-02 20:14:30 -0500

    edit

--- changer_1.sh
+++ changer_1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-gsettings set org.gnome.desktop.background picture-uri file:///home/oberon/Pictures/daily
+gsettings set org.gnome.desktop.background picture-uri file://~/Pictures/daily
 
 gsettings set org.gnome.desktop.background picture-options 'zoom'
